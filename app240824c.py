593dd34fdeaf9d112ea04e81ecf0c1f3abfdc9d7
jotoma1967 <84995332+jotoma1967@users.noreply.github.com>
2024-08-25 01:15:12 +0200

    Add files via upload

--- app240824c.py
+++ app240824c.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
     print("linea 86 ",fecha_actualizacion)
     ##app.run(debug=True)
     ##from waitress import serve
-    serve(app, host="https://project-jpuo.onrender.com/templates/index.html", port = 10000)#0.0.0.0", port=10000)
+    serve(app, host="0.0.0.0", port = 10000)#0.0.0.0", port=10000)
 
     print("linea 90 **********************")
     app.run()
